c669f6e61c6909e779c52009cdee16ca674be799
Eray TURAN <turanerayy@gmail.com>
2023-09-05 21:28:13 +0300

    project files added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# weather-app
+# Weather-App
+This is a weather app in python
+
+
+## Usage
+You have to download pycharm and then you can use this app.
---
+++ weather-app.py
@@ -0,0 +1,59 @@
+from tkinter import *
+from PIL import ImageTk, Image
+import requests
+
+url = "https://api.openweathermap.org/data/2.5/weather"
+apiKey ="2c2de28cf4b7dd448ed7923ef4fb7b1c"
+iconUrl = "https://openweathermap.org/img/wn/{}@2x.png"
+
+def getWeather(city):
+ params ={"q":city, "appid":apiKey, "lang":"tr"}
+ data = requests.get(url, params=params).json()
+ if data:
+     city = data["name"].capitalize()
+     country = data["sys"]["country"]
+     temp = int(data["main"]["temp"]-273.15)
+     icon = data["weather"][0]["icon"]
+     condition = data["weather"][0]["description"]
+     return (city,country,temp,icon,condition)
+
+
+def main():
+     city = cityEntry.get()
+     weather = getWeather(city)
+     if weather:
+         locationLabel["text"]="{},{}".format(weather[0],weather[1])
+         tempLabel["text"]="{}°C".format(weather[2])
+         conditionLabel["text"]=weather[4]
+         icon =ImageTk.PhotoImage(Image.open(requests.get(iconUrl.format(weather[3]),stream=True).raw))
+         iconLabel.configure(image=icon)
+         iconLabel.image =icon
+
+
+
+
+
+app = Tk()
+app.geometry("300x450")
+app.title("Weather App")
+
+cityEntry = Entry(app, justify="center")
+cityEntry.pack(fill=BOTH, ipady=10,padx=18,pady=5)
+cityEntry.focus()
+
+searchButton =Button(app,text="Search",font=("Arial",15),command=main)
+searchButton.pack(fill=BOTH, ipady=10, padx=20)
+
+iconLabel = Label(app)
+iconLabel.pack()
+
+locationLabel = Label(app,font=("Arial",40))
+locationLabel.pack()
+
+tempLabel =Label(app ,font=("Arial",50,"bold"))
+tempLabel.pack()
+
+conditionLabel =Label(app, font=("Arial",20))
+conditionLabel.pack()
+
+app.mainloop()
